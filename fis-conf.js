a768b2bee9993dd6abbdc049b35b6b0e5b4e34aa
qinyang <466168951@qq.com>
2015-10-22 13:20:27 +0800

    add static domain

--- fis-conf.js
+++ fis-conf.js
@@ -7,8 +7,10 @@ fis.match('*.png', {
 //   // url:"/hel$0"
 // });
 fis.match('all.js',{
-	useHash:true
+	useHash:true,
+	domain:"http://7xnpkc.com2.z0.glb.qiniucdn.com"
 })
 fis.match('*.{css,eot,svg,ttf,woff}',{
+	domain:"http://7xnpkc.com2.z0.glb.qiniucdn.com",
 	useHash:true
 })
